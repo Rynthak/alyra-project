bdf58404d114112a3104f440a122d48656a6464a
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-16 16:56:08 +0100

    Add somes fixes for top Menu

---
+++ client/src/components/TopMenu.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <ul class="navbar-nav">
+      <router-link tag="li" class="nav-link" to="/buymonster" exact>
+        <a>
+          <strong>Add new ads</strong>
+        </a>
+      </router-link>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
