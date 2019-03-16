d59ca059f50a40ee81e04abfb45f379400c0233a
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-16 19:00:34 +0100

    QY

---
+++ dapp/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <TopMenu></TopMenu>
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Menu from './components/Menu';
+export default {
+  components: {'TopMenu':Menu},
+  name: 'App'
+}
+</script>
+
+<style>
+    .main-content {
+        padding:20px 40px;
+    }
+    
+</style>
---
+++ dapp/src/components/Menu.vue
@@ -0,0 +1,10 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <ul class="navbar-nav">
+            <router-link tag="li" class="nav-link" to="/newads" exact>
+                <a><strong>new Adss</strong></a>
+            </router-link>
+        </ul>
+    </nav>
+</template>
+ 
