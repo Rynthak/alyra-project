e8a1e5750a9242cc873ca619abc031d691426dbf
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-28 18:53:29 +0100

    Add fixes

--- backend-dapp/app.js
+++ backend-dapp/app.js
@@ -13,17 +13,17 @@ var ethers = require('ethers');
 
 //Config section 
 require('dotenv').config()
-const configNetwork = require('./config/network.json'); 
+const configNetwork = require('./config/network.json');
 const contractConfig = require('./assets/contracts/ClassifiedAds.json');
 
-console.log(configNetwork);
+
 
 
 var app = express();
 
 
 global.db;
- 
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,29 +31,31 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 
 app.use('/', indexRouter);
 app.use('/ads', adsRouter);
- 
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
@@ -61,14 +63,25 @@ app.use(function(err, req, res, next) {
 //STAR db server
 async function startDB() {
     // Open the Database
-    db = await dbLib.openDb();  
-    
+    db = await dbLib.openDb();
+
 }
 startDB();
 
+//Init ethers provider
+var provider = global.provider = new ethers.providers.JsonRpcProvider(configNetwork[process.env.networkid].url + process.env.API_KEY);
+provider.getNetwork().then((r) => {
+    console.log("Ethereum connecté sur ", r)
+    //On écoute l'evenement Ads
+    const contractInstance = new ethers.Contract(contractConfig.networks[process.env.networkid].address, contractConfig.abi, provider)
+    contractInstance.on('Ads', (ads, owner, event) => {
+        //ON PIN LE HASH DE L'EVENT
+        console.log(ads,owner);
+ 
+    });
 
-var provider = global.provider =new ethers.providers.JsonRpcProvider("http://localhost:8545");
 
+});
 
 
 
