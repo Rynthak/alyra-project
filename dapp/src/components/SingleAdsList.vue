abb6c392b1771793a889601b9a84b09b8237a271
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-28 14:19:32 +0100

    Add fixes

--- dapp/src/components/SingleAdsList.vue
+++ dapp/src/components/SingleAdsList.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="single-ads-list">
     <b-media tag="li">
-      <h5 class="mt-0 mb-1">{{ ads.title }}</h5>
+      <router-link :to="{ name: 'SingleAds', params: { id: ads._id }}">
+        <a>
+          <h5 class="mt-0 mb-1">{{ ads.title }}</h5>
+        </a>
+      </router-link>
     </b-media>
   </div>
 </template>
 <script>
 export default {
   name: "single-ads-list",
-  props: ['ads'],
+  props: ["ads"],
   data() {
     return {
       msg: "Single Ads"
