58fc3967de2ab351ce0c88f4eea0aaabd7b5c0d0
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-28 18:09:52 +0100

    delete config

--- dapp/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  ORBITDB_ADDRESS: '"/orbitdb/Qmea5zfeGu6KQUHEeyZ5CKA3EVQiL5nxMhpxxoHXY43Lit/dapp-sells"',
-  API_URL: '"http://localhost:3000"'
-})
--- dapp/config/prod.env.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-module.exports = {
-  NODE_ENV: '"production"',
-  ORBITDB_ADDRESS: '',
-  API_URL: ''
-}
