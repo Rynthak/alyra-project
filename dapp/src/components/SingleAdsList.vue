aac319d73caa5c6905bae7184cb1aac9fe5fa731
Rynthak <mehdi.boutoubat@gmail.com>
2019-03-29 14:04:58 +0100

    Add fixes

--- dapp/src/components/SingleAdsList.vue
+++ dapp/src/components/SingleAdsList.vue
@@ -7,10 +7,15 @@
             <font-awesome-icon :icon="[ 'fas', 'clock' ]" fixed-width/>
             {{ ads.date_add | moment("from", "now") }}
           </b-badge>
+          <b-badge variant="warning">
+            <font-awesome-icon :icon="[ 'fas', 'id-card-alt' ]" fixed-width/>
+            {{  ads._id }}
+          </b-badge>         
           <b-badge variant="light">
             <flag :iso="ads.city.country"/>
             {{ ads.city.name }}
           </b-badge>
+          <b-badge variant="success" v-if="ads.owner.toLowerCase() == coinbase.toLowerCase()">My Ads</b-badge>
           <h5 class="mt-0 mb-1">{{ ads.title }}</h5>
         </a>
       </router-link>
@@ -23,7 +28,8 @@ export default {
   props: ["ads"],
   data() {
     return {
-      msg: "Single Ads"
+      msg: "Single Ads",
+      coinbase: this.$store.state.web3.coinbase
     };
   }
 };
